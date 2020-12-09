a620668f38a1e506257653f7dbfc827e32c12afd
Niko-MNW <47422338+NikoMNW@users.noreply.github.com>
2020-12-09 16:52:26 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# HTH-MN-setup
+# AXE-MN-setup UBUNTU 16.04 ONLY!! V1.6.1.1 DIP003 enabled
+<br>
+<li>sudo apt-get update
+<li>sudo apt-get install git
+<br><br>
+<li> If you need the script to make a node <b> WITH SWAP</b> Use this line to install
+<li> This is typical usage with VULTR and Digital Ocean 
+<li>Copy the entire line below and paste into your ssh session or vps terminal
+<li>git clone https://github.com/twystidceed/AXE-MN-setup.git && cd AXE-MN-setup && bash axe-setup-swap.sh
+<br><br>
+<li> For installation on VPS with preset swap use this line - <b>This does NOT install Swap</b> 
+<li> This is typical for more specialized nodes for advanced users
+<li>Copy the entire line below and paste into your ssh session or vps terminal
+<li>git clone https://github.com/twystidceed/AXE-MN-setup.git && cd AXE-MN-setup && bash axe-setup-noswap.sh
+<br><br>
+<li><b> Anyone using my script can update their node quickly by using the following line. </b>
+<li> Just copy and paste the line below - this will update your existing files and restart the daemon on 1.4.1
+<li>cd AXE-MN-setup && git fetch --all && git reset --hard origin/master && bash update.sh
---
+++ update.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+# AXE Masternode Update Script for Ubuntu 16.04 LTS
+#
+# Script will attempt to Stop and remove old install and update the daemon
+# and generate masternode private key unless specified in command line
+#
+# Usage:
+# bash update.sh 
+#
+
+#Color codes
+RED='\033[0;91m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+#Clear keyboard input buffer
+function clear_stdin { while read -r -t 0; do read -r; done; }
+
+#Delay script execution for N seconds
+function delay { echo -e "${GREEN}Sleep for $1 seconds...${NC}"; sleep "$1"; }
+
+#Stop daemon if it's already running
+
+function stop_daemon {
+    if pgrep -x 'axed' > /dev/null; then
+        echo -e "${YELLOW}Attempting to stop axed${NC}"
+        axe-cli stop
+        delay 60
+        if pgrep -x 'axe' > /dev/null; then
+            echo -e "${RED}axed daemon is still running!${NC} \a"
+            echo -e "${RED}Attempting to kill...${NC}"
+            pkill axed
+            delay 60
+            if pgrep -x 'axed' > /dev/null; then
+                echo -e "${RED}Can't stop axed! Reboot and try again...${NC} \a"
+                exit 2
+            fi
+        fi
+    fi
+}
+
+#Function detect_ubuntu
+
+ if [[ $(lsb_release -d) == *16.04* ]]; then
+   UBUNTU_VERSION=16
+else
+   echo -e "${RED}You are not running Ubuntu 16.04, Installation is cancelled.${NC}"
+   exit 1
+
+fi
+
+
+#Check for updates
+
+sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+sudo apt-get -y update
+
+#remove existing daemon 
+ stop_daemon
+rm -rf ~/AXE-MN-setup/axecore*
+rm /usr/bin/axe*
+
+#Installing Daemon
+ cd ~
+wget https://github.com/AXErunners/axe/releases/download/v1.6.1.1/axecore-1.6.1.1-x86_64-linux-gnu.tar.gz
+tar -xzf axecore-1.6.1.1-x86_64-linux-gnu.tar.gz -C ~/AXE-MN-setup
+rm -rf axecore-1.6.1.1-x86_64-linux-gnu.tar.gz
+ 
+ # Deploy binaries to /usr/bin
+ sudo rm ~/AXE-MN-setup/axecore-1.6.1.1/bin/axe-qt
+ sudo rm ~/AXE-MN-setup/axecore-1.6.1.1/bin/test*
+ sudo cp ~/AXE-MN-setup/axecore-1.6.1.1/bin/axe* /usr/bin/
+ sudo chmod 755 -R ~/AXE-MN-setup
+ sudo chmod 755 /usr/bin/axe*
+ 
+
+    #Starting daemon
+    axed -daemon -reindex
+echo -ne '[##                 ] (15%)\r'
+sleep 10
+echo -ne '[######             ] (30%)\r'
+sleep 10
+echo -ne '[########           ] (45%)\r'
+sleep 10
+echo -ne '[############       ] (67%)\r'
+sleep 10
+echo -ne '[################   ] (72%)\r'
+sleep 10
+echo -ne '[###################] (100%)\r'
+echo -ne '\n'
+
+
+echo -e "========================================================================
+${YELLOW}Masternode Update is complete!${NC}
+========================================================================"
+
+delay 30
+# Run axemon.sh
+axemon.sh
+
+# EOF
